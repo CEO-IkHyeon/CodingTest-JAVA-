74c4208f64ab68b8fb81e73c5719a0ecabc3552e
IkHeyonCho <andantej281@gmail.com>
2024-01-24 07:07:08 +0900

    10158 푸는중

---
+++ src/main/java/part01/ch01_String/Main10158.java
@@ -0,0 +1,22 @@
+package part01.ch01_String;
+
+import java.util.Scanner;
+
+public class Main10158 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int w = sc.nextInt();
+        int h = sc.nextInt();
+        int p = sc.nextInt();
+        int q = sc.nextInt();
+        int t = sc.nextInt();
+        int[][] direction = {{1, 1}, {-1, 1}, {-1, -1}, {-1, 1} };
+
+        for (int i = 0; i < t; i++) {
+            int nextX = p+1;
+            int nextY = q+1;
+            if (nextX > )
+        }
+
+    }
+}
